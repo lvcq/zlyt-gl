7311280a782499d2a273dfa67fdf9bc60eb95d99
lvcq617 <lvcq617@aliyun.com>
2020-12-26 20:13:46 +0800

    add unit test frameworks:karma+mocha+chai

---
+++ tests/scene.spec.ts
@@ -0,0 +1,10 @@
+describe("HelloComponent", () => {
+
+  it("should say 'Hello world!'", () => {
+    expect('hello world').to.be.equal('hello world');
+  });
+
+  it("should be true",()=>{
+    expect('s'==='s').to.be.equal(true);
+  })
+});
